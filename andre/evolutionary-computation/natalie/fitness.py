4e149249457b0197e2ba53d84b41eeaaf1876a52
bravonatalie <natalie.bravo@outlook.com>
2021-01-14 19:23:00 -0300

    refactoring 1.0

--- andre/evolutionary-computation/natalie/fitness.py
+++ andre/evolutionary-computation/natalie/fitness.py
@@ -22,40 +22,6 @@ class FitnessValues:
   def __repr__(self):
     return f'---- FitnessValues ----\n concepts_covered: {self.concepts_covered}, difficulty: {self.difficulty}, total_time: {self.total_time}, materials_balancing: {self.materials_balancing}, learning_style: {self.learning_style}, total: {self.total} \n'
 
-  @classmethod
-  def concepts_covered_fn(cls, student_id, materials_concepts):
-    # conceitos que foram indicados aos alunos
-    objectives = cls.default_instance.objectives[student_id]
-
-    all_covered_concepts = np.logical_or.reduce(materials_concepts).astype(int)
-    over_covered_concepts = np.copy(all_covered_concepts)
-    over_covered_concepts[objectives] = 0
-
-    under_covered_concepts = objectives & (all_covered_concepts == 0)
-
-    result = (over_covered_concepts.sum() + (FitnessValues.default_instance.missing_concepts_coeficient * under_covered_concepts.sum()))
-
-    return result
-
-  @classmethod
-  def materials_balancing_fn(cls, student_id, materials_concepts):
-    objectives = cls.default_instance.objectives[student_id].astype(bool)
-
-    amount_of_materials_for_all_concepts = materials_concepts.astype(bool).sum()
-    mean_concepts_per_objective = amount_of_materials_for_all_concepts / objectives.sum()
-
-    amount_of_materials_per_objectives = materials_concepts.sum(axis=0)
-    amount_of_materials_per_objectives = amount_of_materials_per_objectives[amount_of_materials_per_objectives != 0]
-
-    # if(student_id == 5):
-    #   print("amount_of_materials_for_all_concepts: ", materials_concepts.astype(bool))
-    #   print("mean_concepts_per_objective: ", mean_concepts_per_objective)
-    #   print("\n")
-
-    distance_from_mean = np.abs(amount_of_materials_per_objectives - mean_concepts_per_objective)
-
-    return distance_from_mean.sum()
-
   @staticmethod
   def get_Instance(materials_concepts):
     individual = list()
@@ -65,11 +31,6 @@ class FitnessValues:
 
   @classmethod
   def fitness_fn(cls, student_id, materials_concepts):
-    # concepts_covered_objective = cls.concepts_covered_fn(
-    #     student_id, materials_concepts)
-    # materials_balancing_objective = cls.materials_balancing_fn(
-    #     student_id, materials_concepts)
-
     individual = FitnessValues.get_Instance(materials_concepts)
 
     concepts_covered_objective = objective.concepts_covered_function(individual, cls.default_instance, student_id)
--- andre/evolutionary-computation/natalie/main.py
+++ andre/evolutionary-computation/natalie/main.py
@@ -1,61 +1,55 @@
-from solution import Solution
-# from problemDefinition import DiscreteOpt
-# from grasp import grasp
-# import numpy as np
-# from report.report import createReport
-# from collections import defaultdict
-# import matplotlib.pyplot as plt
-import acs.objective as objective
-from acs.instance import Instance
-import numpy as np
-from fitness import FitnessValues
 import os
+import numpy as np
+import matplotlib.pyplot as plt
+from collections import defaultdict
+
+from solution import Solution
+from grasp import grasp
+from problemDefinition import DiscreteOpt
+from acs.instance import Instance
+from report.report import createReport
 
 dir = os.path.dirname(__file__)
 
 
-# new_repository = dict(zip(np.arange(0, 284), np.zeros(284, dtype=int)))
+new_repository = dict(zip(np.arange(0, 284), np.zeros(284, dtype=int)))
 """ Novo repositório gerado com as modificações """
 
-# all_new_material = {
-#     "total": len(new_repository) - 1,
-#     "materials": []
-# }
+all_new_material = {
+    "total": len(new_repository) - 1,
+    "materials": []
+}
 """ materiais novos e o tamanho novo do repositório """
 
 
-# def add_to_allNewMaterial(covered_concepts, parent_material_id):
-""" adiciona um material novo se não existente """
-#     for mat in all_new_material["materials"]:
-#         if np.array_equal(covered_concepts, mat[1]):
-#             return mat
+def add_to_allNewMaterial(covered_concepts, parent_material_id):
+    """ adiciona um material novo se não existente """
+    for mat in all_new_material["materials"]:
+        if np.array_equal(covered_concepts, mat[1]):
+            return mat
     
-#     id = all_new_material["total"] + 1
-#     material = (id, covered_concepts, parent_material_id)
-#     all_new_material["materials"].append(material)
-#     all_new_material["total"] += 1
-
-#     return material
-
+    id = all_new_material["total"] + 1
+    material = (id, covered_concepts, parent_material_id)
+    all_new_material["materials"].append(material)
+    all_new_material["total"] += 1
+    return material
 
 
-# def add_count_to_dict(dic, value):
-""" dicionário com contagem de value """
-#     if value in dic:
-#         dic[value] += 1
-#     else:
-#         dic[value] = 1
-#     return dic
+def add_count_to_dict(dic, value):
+    """ dicionário com contagem de value """
+    if value in dic:
+        dic[value] += 1
+    else:
+        dic[value] = 1
+    return dic
 
 
 instance_file = os.path.join(dir,'..','instances','real','instance.txt')
-#instance_file = '/home/bravo/Documents/TCC/TCC-repo/andre/evolutionary-computation///'
 
 studentsXselectedMaterials_file = os.path.join(dir,'..', 'studentXselectedMaterials-02-06-2020.csv')
-#studentsXselectedMaterials_file = '/home/bravo/Documents/TCC/TCC-repo/andre/evolutionary-computation/studentXselectedMaterials-02-06-2020.csv'
 
 studentsXfitness_file = os.path.join(dir,'..', 'student_fitnessFunction-02-06-2020.csv')
-#studentsXfitness_file = '/home/bravo/Documents/TCC/TCC-repo/andre/evolutionary-computation/student_fitnessFunction-02-06-2020.csv'
+
 
 """ --------------------------------------------------- """
 
@@ -64,166 +58,146 @@ studentsXfitness_file = os.path.join(dir,'..', 'student_fitnessFunction-02-06-20
 
 initialSolution = Solution(instance_file, studentsXselectedMaterials_file, studentsXfitness_file)
 
-# improvedSolution = []
-#print(initialSolution.students_list[5])
+improvedSolution = []
 
-student = initialSolution.students_list[5]
-print("Aluno 5 materials_balancing:", FitnessValues.materials_balancing_fn(student.student_id, student.materials_concepts))
-#print(student.materials_concepts)
-individual5 = list()
-for mat in student.materials_concepts:
-    if(mat.sum() == 0):
-        individual5.append(False)
-    else:
-        individual5.append(True)
+students_list_report = list()  # lista de alunos no formato exigido para imprimir o relatório
 
-instance = Instance.load_from_file(instance_file)
+total_removed_materials = dict()  # key: material removido, value: quantidades de vezes 
+total_added_materials = dict()  # key: material removido, value: quantidades de vezes  key: material adicionado, value: quantidades de vezes 
+removed_materials_concepts = defaultdict(set)  # key: material removido, value: todos conceitos retirados
+added_materials_concepts = defaultdict(set)  # key: material adicionado, value: todos conceitos adicionados 
 
-print("Aluno 5 materials_balancing_andre: ",  objective.materials_balancing_function(np.array(individual5), instance, 5))
+for student in initialSolution.students_list:
+    if(student.fitnessConcepts != 0.0):
+        problem = DiscreteOpt(student)
+        materials_concepts, fitness = grasp(problem, max_Iterations=100, alfa=0.8, seed=0)
 
-# student = initialSolution.students_list[23]
-# print("Aluno 23 materials_balancing:" , FitnessValues.materials_balancing_fn(student.student_id, student.materials_concepts))
+        with open('./natalie/iteration_log.txt', 'a') as f:
+            print(f'------ Aluno: {student.student_id}', file=f)
 
 
+        modified_materials = list()
+        for index, material_zip in enumerate(zip(student.materials_concepts, materials_concepts)):
+            old_material = material_zip[0]
+            new_material = material_zip[1]
 
-# students_list_report = list()  # lista de alunos no formato exigido para imprimir o relatório
+            removed_concepts = list()
+            added_concepts = list()
 
-# total_removed_materials = dict()  # key: material removido, value: quantidades de vezes 
-# total_added_materials = dict()  # key: material removido, value: quantidades de vezes  key: material adicionado, value: quantidades de vezes 
-# removed_materials_concepts = defaultdict(set)  # key: material removido, value: todos conceitos retirados
-# added_materials_concepts = defaultdict(set)  # key: material adicionado, value: todos conceitos adicionados 
+            if not np.array_equal(old_material, new_material):
+                material_id = index
 
-# for student in initialSolution.students_list:
-#     if(student.fitnessConcepts != 0.0):
-#         problem = DiscreteOpt(student)
-#         materials_concepts, fitness = grasp(problem, max_Iterations=100, alfa=0.8, seed=0)
+                diff = new_material - old_material
+                changed_concepts_index = np.where(diff != 0)[0]
 
-#         with open('./natalie/iteration_log.txt', 'a') as f:
-#             print(f'------ Aluno: {student.student_id}', file=f)
+                for conceito in changed_concepts_index:
+                    mat = diff[conceito]
+                    if(mat < 0):
+                        removed_concepts.append(conceito)
+                    else:
+                        added_concepts.append(conceito)
 
-
-#         modified_materials = list()
-#         for index, material_zip in enumerate(zip(student.materials_concepts, materials_concepts)):
-#             old_material = material_zip[0]
-#             new_material = material_zip[1]
-
-#             removed_concepts = list()
-#             added_concepts = list()
-
-#             if not np.array_equal(old_material, new_material):
-#                 material_id = index
-
-#                 diff = new_material - old_material
-#                 changed_concepts_index = np.where(diff != 0)[0]
-
-#                 for conceito in changed_concepts_index:
-#                     mat = diff[conceito]
-#                     if(mat < 0):
-#                         removed_concepts.append(conceito)
-#                     else:
-#                         added_concepts.append(conceito)
-
-#                 changed_material = {"material_id": material_id,
-#                                     "removed_concepts": removed_concepts,
-#                                     "added_concepts": added_concepts
-#                                     }
+                changed_material = {"material_id": material_id,
+                                    "removed_concepts": removed_concepts,
+                                    "added_concepts": added_concepts
+                                    }
                             
-#                 modified_materials.append(changed_material)
+                modified_materials.append(changed_material)
 
-#                 """ start: chart report """
+                """ start: chart report """
                 
-#                 total_removed_materials = add_count_to_dict(total_removed_materials, material_id)
+                total_removed_materials = add_count_to_dict(total_removed_materials, material_id)
 
-#                 if new_material.sum() > 0:
-#                     new_learning_material = add_to_allNewMaterial(new_material, material_id)
-#                     total_added_materials = add_count_to_dict(total_added_materials, new_learning_material[0])
+                if new_material.sum() > 0:
+                    new_learning_material = add_to_allNewMaterial(new_material, material_id)
+                    total_added_materials = add_count_to_dict(total_added_materials, new_learning_material[0])
 
 
-#                 for concept in removed_concepts:
-#                     removed_materials_concepts[material_id].add(concept)
+                for concept in removed_concepts:
+                    removed_materials_concepts[material_id].add(concept)
                 
-#                 for concept in added_concepts:
-#                     removed_materials_concepts[material_id].add(concept)
+                for concept in added_concepts:
+                    removed_materials_concepts[material_id].add(concept)
                 
-#                 """end:  chart report """
-#             # else:
-#             #     for index, mat in enumerate(materials_concepts):
-#             #         if materials_concepts[mat].sum() > 0:
-#             #             new_repository = add_count_to_dict(new_repository, index)
+                """end:  chart report """
+            # else:
+            #     for index, mat in enumerate(materials_concepts):
+            #         if materials_concepts[mat].sum() > 0:
+            #             new_repository = add_count_to_dict(new_repository, index)
 
-#         student = {"student_id": student.student_id,
-#                    "fitness_before": student.fitnessConcepts,
-#                    "fitness_now": fitness,
-#                    "modified_materials": len(modified_materials),
-#                    "materials": modified_materials
-#                    }
+        student = {"student_id": student.student_id,
+                   "fitness_before": student.fitnessConcepts,
+                   "fitness_now": fitness,
+                   "modified_materials": len(modified_materials),
+                   "materials": modified_materials
+                   }
 
-#         students_list_report.append(student)
+        students_list_report.append(student)
 
-#     else:
-#         materials_concepts = student.materials_concepts
+    else:
+        materials_concepts = student.materials_concepts
 
-#         """ start: counting frequency of use """
-#         # for index, mat in enumerate(materials_concepts):
-#         #     if materials_concepts[mat].sum() > 0:
-#         #         new_repository = add_count_to_dict(new_repository, index)
+        """ start: counting frequency of use """
+        # for index, mat in enumerate(materials_concepts):
+        #     if materials_concepts[mat].sum() > 0:
+        #         new_repository = add_count_to_dict(new_repository, index)
                 
-#         """ end: counting frequency of use """
+        """ end: counting frequency of use """
 
 
-# improvedSolution.append(materials_concepts)
+improvedSolution.append(materials_concepts)
 
 
 
-# y = list(total_removed_materials.keys())
-# x = list(total_removed_materials.values())
+y = list(total_removed_materials.keys())
+x = list(total_removed_materials.values())
 
-# y1 = list(total_added_materials.keys())
-# x1 = list(total_added_materials.values())
+y1 = list(total_added_materials.keys())
+x1 = list(total_added_materials.values())
 
-# filename = "Removed_Materials&Added_Materials.png"
+filename = "Removed_Materials&Added_Materials.png"
 
-# fig, (ax1, ax2) = plt.subplots(1, 2)
+fig, (ax1, ax2) = plt.subplots(1, 2)
 
-# ax1.barh(np.arange(len(y)), x, 0.6)
-# ax1.set_xticks(np.arange(min(x), 2 * max(x) + 1, 1))
-# ax1.set_yticks(np.arange(len(y)))
-# ax1.set_yticklabels(y)
-# ax1.set_ylabel('Removed Materials')
-# ax1.set_xlabel('Number of students')
+ax1.barh(np.arange(len(y)), x, 0.6)
+ax1.set_xticks(np.arange(min(x), 2 * max(x) + 1, 1))
+ax1.set_yticks(np.arange(len(y)))
+ax1.set_yticklabels(y)
+ax1.set_ylabel('Removed Materials')
+ax1.set_xlabel('Number of students')
 
-# ax2.barh(np.arange(len(y1)), x1, 0.3)
-# ax2.set_xticks(np.arange(min(x1), 2 * max(x1) + 1, 1))
-# ax2.set_yticks(np.arange(len(y1)))
-# ax2.set_yticklabels(y1)
-# ax2.set_ylabel('Added Materials')
-# ax2.set_xlabel('Number of students')
-# fig.tight_layout(pad=4.0)
-# plt.savefig('./natalie/images/' + filename)
-
-
-# # for mat in new_repository:
-# #     if new_repository[mat] == 0:
-# #         print(mat)
+ax2.barh(np.arange(len(y1)), x1, 0.3)
+ax2.set_xticks(np.arange(min(x1), 2 * max(x1) + 1, 1))
+ax2.set_yticks(np.arange(len(y1)))
+ax2.set_yticklabels(y1)
+ax2.set_ylabel('Added Materials')
+ax2.set_xlabel('Number of students')
+fig.tight_layout(pad=4.0)
+plt.savefig('./natalie/images/' + filename)
 
 
-# # new_repository.update(total_added_materials)
-# # print(new_repository)
-# # print(len(new_repository))
-# # y2 = list(new_repository.keys())
-# # x2 = list(new_repository.values())
-# # fig, ax = plt.subplots()
-# # ax.barh(np.arange(len(y2)), x2, 0.0001)
-# # ax.set_xticks(np.arange(min(x2), max(x2) + 1, 1))
-# # ax.set_yticks(np.arange(len(y2)))
-# # ax.set_yticklabels(y2)
-# # plt.ylabel('Novo repositório')
-# # plt.xlabel('Quantidade de alunos')
-# # plt.savefig('./natalie/images/repositórioXquantidadeAlunos.png')
+# for mat in new_repository:
+#     if new_repository[mat] == 0:
+#         print(mat)
 
-# # create report
 
-# template_vars = {"total_students": 24, "students": students_list_report, "number_of_modified_students": len(students_list_report), "chart1": './natalie/images/' + filename }
-# filename = "grasp_report"
+# new_repository.update(total_added_materials)
+# print(new_repository)
+# print(len(new_repository))
+# y2 = list(new_repository.keys())
+# x2 = list(new_repository.values())
+# fig, ax = plt.subplots()
+# ax.barh(np.arange(len(y2)), x2, 0.0001)
+# ax.set_xticks(np.arange(min(x2), max(x2) + 1, 1))
+# ax.set_yticks(np.arange(len(y2)))
+# ax.set_yticklabels(y2)
+# plt.ylabel('Novo repositório')
+# plt.xlabel('Quantidade de alunos')
+# plt.savefig('./natalie/images/repositórioXquantidadeAlunos.png')
 
-# createReport(filename, template_vars)
+# create report
+
+template_vars = {"total_students": 24, "students": students_list_report, "number_of_modified_students": len(students_list_report), "chart1": './natalie/images/' + filename }
+filename = "grasp_report"
+
+createReport(filename, template_vars)
